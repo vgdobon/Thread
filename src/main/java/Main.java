452044f8f2ebb679f69488bfbb6068f7e9419c88
vgdobon <vgdobon91@gmail.com>
2020-09-18 11:06:42 +0200

    Threads

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,44 @@
 import com.tecnara.thread.ChildThread;
 
+
+//    Thread de clase pública
+//    extiende
+//    Implementos de objetos Runnable
+//    Un hilo es un hilo de ejecución en un programa. La máquina virtual Java permite que una aplicación tenga varios subprocesos de ejecución ejecutándose al mismo tiempo.
+//    Cada hilo tiene una prioridad. Los subprocesos con mayor prioridad se ejecutan con preferencia a los subprocesos con menor prioridad.
+//    Cada subproceso también puede estar marcado o no como un demonio.
+//    Cuando el código que se ejecuta en algún subproceso crea un nuevo Threadobjeto, el nuevo subproceso tiene su prioridad establecida inicialmente igual a la prioridad del subproceso de creación, y es un subproceso de demonio si y solo si el subproceso de creación es un demonio.
+//
+//    Cuando se inicia una máquina virtual Java, suele haber un único hilo que no es demonio (que normalmente llama al método denominado mainde alguna clase designada).
+//    La máquina virtual de Java continúa ejecutando subprocesos hasta que ocurre cualquiera de las siguientes situaciones:
+//
+//    Se ha llamado al exitmétodo de clase Runtimey el administrador de seguridad ha permitido que se lleve a cabo la operación de salida.
+//    Todos los subprocesos que no son subprocesos daemon han muerto, ya sea al regresar de la llamada al runmétodo o al lanzar una excepción que se propaga más allá del run método.
+//    Hay dos formas de crear un nuevo hilo de ejecución. Uno es declarar una clase como una subclase de Thread. Esta subclase debe anular el runmétodo de clase Thread.
+//    Luego, se puede asignar e iniciar una instancia de la subclase.
+
+
 public class Main {
-
     public static void main(String[] args) {
-        ChildThread chil1 = new ChildThread();
-        ChildThread chil2 = new ChildThread();
-        ChildThread chil3 = new ChildThread();
+        ChildThread child1 = new ChildThread(1000);
+        ChildThread child2 = new ChildThread(10000);
+        ChildThread child3 = new ChildThread(5000);
 
-        chil1.start();
-        chil2.start();
-        chil3.start();
+
+        child1.start();
+        System.out.println("Lanzado child1.start()");
+
+        child2.start();
+        System.out.println("Lanzado child2.start()");
+
+        child3.start();
+        System.out.println("Lanzado child3.start()");
+
+        try {
+            child2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Termina el Main");
     }
 }
--- src/main/java/com/tecnara/thread/ChildThread.java
+++ src/main/java/com/tecnara/thread/ChildThread.java
@@ -2,8 +2,26 @@ package com.tecnara.thread;
 
 public class ChildThread extends Thread{
 
+
+    int segundos;
+
+    public ChildThread(int segundos){
+
+        this.segundos =  segundos;
+
+    }
+
     @Override
     public void run(){
-        System.out.println("Ejecutando el hilo con el id: " + this.getId());
+
+        System.out.println("Ejecutando el hilo" + this.getName()+" con el id: " + this.getId());
+
+        try {
+            this.join(segundos);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
